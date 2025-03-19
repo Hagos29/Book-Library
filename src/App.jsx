710e43350efc69685610849908ee9f7876a6119a
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-03-19 12:53:58 +0300

    initial commit add Book Library

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-center font-bold p-8 text-2xl'><span className='text-amber-300 '>Book</span> Library</h1>
+    </>
+  )
+}
+
+export default App
